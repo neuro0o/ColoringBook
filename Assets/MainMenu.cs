1229dd890bdf1a23e8052bf14646953d54c83eac
neuro0o <146324467+neuro0o@users.noreply.github.com>
2025-01-18 14:51:38 +0800

    last commit
    - need to setup vuforia

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
-{
+
+public class MainMenu : MonoBehaviour {
 
     public void PlayGame () {
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame () {
-        Debug.Log ("Quit Game");
+        Debug.Log ("QUIT");
         Application.Quit();
     }
+    
 }
